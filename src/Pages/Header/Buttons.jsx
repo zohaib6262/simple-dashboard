e99926c3c682143dd7d3ae6a1ebd86ab86c191b7
zohaib6262 <zohaibbinarhraaf@gmail.com>
2024-04-15 13:51:22 -0700

    commit

--- src/Pages/Header/Buttons.jsx
+++ src/Pages/Header/Buttons.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Buttons = () => {
   return (
-    <div>
-      <button className="bg-sky-600 hover:bg-sky-700 text-white px-4 py-3 rounded-s-lg">
+    <div className="flex flex-nowrap">
+      <button className="bg-sky-600 hover:bg-sky-700 text-white px-2 py-2 rounded-s-lg sm:px-0">
         E-commerce
       </button>
       <button className="bg-sky-100 py-3 px-4 rounded-e-lg">Flipping</button>
