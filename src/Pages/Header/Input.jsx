0f39489dfe20a5bdcdcc9e8604478b7dc182dbd1
zohaib6262 <zohaibbinarhraaf@gmail.com>
2024-04-26 10:11:05 -0700

    commit

--- src/Pages/Header/Input.jsx
+++ src/Pages/Header/Input.jsx
@@ -10,7 +10,7 @@ function Input() {
           placeholder="Search..."
           className="px-2 py-2 w-full sm:w-auto outline-none bg-[#EDF8FD]"
         />
-        <div className="m-auto relative lg:left-5 sm:-left-2 text-white bg-[#008ECC] focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-3 py-1 search-icon-container">
+        <div className="m-auto relative lg:left-5 sm:-left-4 text-white bg-[#008ECC] focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-3 py-1 search-icon-container">
           <Search className="w-4 h-6 " />
         </div>
       </form>
