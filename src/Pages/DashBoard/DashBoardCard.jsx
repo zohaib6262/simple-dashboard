58b681fee3facda1d9adafc4a03bf2846f69dfa5
zohaib6262 <zohaibbinarhraaf@gmail.com>
2024-04-26 09:47:20 -0700

    commit

--- src/Pages/DashBoard/DashBoardCard.jsx
+++ src/Pages/DashBoard/DashBoardCard.jsx
@@ -6,16 +6,18 @@ const DashBoardCard = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {DashBoardData.map((card, index) => (
           <div className="bg-white rounded-lg p-4 shadow-md hover:shadow-lg transition duration-300 ease-in-out">
-            <div className="flex items-center justify-between">
-              <h2 className="ml-0 text-base  text-[#202224] font-medium">
-                {card.title}
-              </h2>
-              {card.icon}
+            <div className="flex justify-between">
+              <div>
+                <h2 className="ml-0 text-base  text-[#202224] font-medium">
+                  {card.title}
+                </h2>
+              </div>
+              <div>{card.icon}</div>
             </div>
             <div className="mt-1">
               <p className=" font-bold text-[#202224] text-2xl">{card.value}</p>
             </div>
-            <div className="mt-2 flex gap-2 justify-center items-center">
+            <div className="mt-2 flex gap-2 ">
               <p>{card.upDown}</p>
               <p
                 className={`text-sm inline-block ${
