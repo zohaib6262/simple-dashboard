98d5557742c131872d89568cf1c500126a0bb6bb
zohaib6262 <zohaibbinarhraaf@gmail.com>
2024-04-28 09:37:50 -0700

    commit

--- src/Pages/Orders/Modal.jsx
+++ src/Pages/Orders/Modal.jsx
@@ -19,7 +19,7 @@ const Modal = ({ open, item, close, onConfirm, onCancel }) => {
           className="fixed inset-0 bg-[#000000] bg-opacity-20 overflow-y-auto h-full w-full m-auto"
           id="my-modal"
         >
-          <div className="relative top-44 h-64 my-auto mx-auto p-3 border w-1/2 shadow-lg rounded-md bg-white">
+          <div className="relative top-44 lg:h-64 sm:h-1/2 my-auto mx-auto p-3 border w-1/2 shadow-lg rounded-md bg-white">
             <div className="mt-3 text-center">
               <h3 className="text-lg leading-6 font-medium text-[#202224]">
                 Generate Orders Report
@@ -30,10 +30,10 @@ const Modal = ({ open, item, close, onConfirm, onCancel }) => {
                 </p>
               </div>
 
-              <div className="flex justify-center gap-8 items-center mt-1">
+              <div className="flex flex-wrap justify-center gap-8 items-center mt-1">
                 <form className="flex  border-hidden rounded-lg overflow-visible bg-[#EDF8FD]">
                   <input
-                    className="px-4 rounded-xl py-2 w-40 bg-[#EDF8FD] outline-none"
+                    className="px-4 rounded-xl py-2 w-24 bg-[#EDF8FD] outline-none"
                     type="text"
                     placeholder="From"
                   />
@@ -41,9 +41,9 @@ const Modal = ({ open, item, close, onConfirm, onCancel }) => {
                     <Image />
                   </div>
                 </form>
-                <form className="flex flex-wrap  border-hidden rounded-lg overflow-visible bg-[#EDF8FD]">
+                <form className="flex border-hidden rounded-lg overflow-visible bg-[#EDF8FD]">
                   <input
-                    className="px-4 rounded-xl py-2 w-40 bg-[#EDF8FD] outline-none"
+                    className="px-4 rounded-xl py-2 w-24 bg-[#EDF8FD] outline-none"
                     type="text"
                     placeholder="To"
                   />
@@ -53,23 +53,27 @@ const Modal = ({ open, item, close, onConfirm, onCancel }) => {
                 </form>
               </div>
 
-              <div className="flex flex-wrap justify-center gap-8 mt-5  items-center px-4 py-3">
-                <button
-                  onClick={() => {
-                    onCancel();
-                  }}
-                  className="px-4 py-2 bg-[#FF6666] text-white text-base font-medium rounded-md w-1/3 shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400"
-                >
-                  Download PDF
-                </button>
-                <button
-                  onClick={() => {
-                    onCancel();
-                  }}
-                  className="px-4 py-2 bg-[#54C48D] text-white text-base font-medium rounded-md w-1/3 shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400"
-                >
-                  Download CSV
-                </button>
+              <div className="flex flex-wrap justify-center gap-8 mt-5 items-center px-4 py-3">
+                <div>
+                  <button
+                    onClick={() => {
+                      onCancel();
+                    }}
+                    className="px-4 py-2 bg-[#FF6666] text-white text-base font-medium rounded-md w-36 shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400"
+                  >
+                    Download PDF
+                  </button>
+                </div>
+                <div>
+                  <button
+                    onClick={() => {
+                      onCancel();
+                    }}
+                    className="px-4 py-2 bg-[#54C48D] text-white text-base font-medium rounded-md w-36  shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400"
+                  >
+                    Download CSV
+                  </button>
+                </div>
               </div>
             </div>
           </div>
